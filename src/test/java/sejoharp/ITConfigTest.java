97b55739f961ec9674a80262cef0ae91edcc69b1
joscha <joscha@harpeng.org>
2017-11-30 22:50:59 +0100

    switch to gradle and update to spring boot 1.5.9

--- src/test/java/sejoharp/ITConfigTest.java
+++ src/test/java/sejoharp/ITConfigTest.java
@@ -3,14 +3,14 @@ package sejoharp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(App.class)
+@SpringBootTest
 public class ITConfigTest {
     @Autowired
     Config config;
